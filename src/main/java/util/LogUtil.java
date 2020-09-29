267b74645df40000288ebb96d20cdc5adcd21f1c
znakarik <znakarik@icloud.com>
2020-09-29 22:23:49 +0300

    init

---
+++ src/main/java/util/LogUtil.java
@@ -0,0 +1,13 @@
+package util;
+
+import io.qameta.allure.Step;
+
+public class LogUtil {
+
+    private LogUtil(){}
+
+    @Step("{0}")
+    public static void log(String message){
+
+    }
+}
--- src/test/java/JenkinsTest.java
+++ src/test/java/JenkinsTest.java
@@ -1,16 +1,29 @@
 import io.qameta.allure.Step;
 import org.junit.Assert;
 import org.junit.Test;
+import ru.yandex.qatools.allure.annotations.TestCaseId;
+import util.LogUtil;
 
 public class JenkinsTest {
 
-    @Step("Проверяем равен ли один двум")
-    public void initialTest(){
-        Assert.assertTrue(1 == 2);
+    @Step("Проверяем 1 == 1")
+    public void initialTest() {
+        Assert.assertEquals(1, 1);
     }
 
     @Test
-    public void test(){
+    @TestCaseId("1")
+    public void test() {
+        LogUtil.log("Начинается первый тест");
         initialTest();
+        LogUtil.log("Закончился первый тест");
+    }
+
+    @Test
+    @TestCaseId("2")
+    public void secondTest(){
+        LogUtil.log("Начинается второй тест");
+        initialTest();
+        LogUtil.log("Закончился второй тест");
     }
 }
